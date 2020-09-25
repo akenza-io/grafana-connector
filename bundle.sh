7a068ea34a2baded13e40fbcf6c6b6d72a579687
Marius Giger <gigermarius@gmail.com>
2020-09-25 19:26:35 +0200

    use cp directly

--- bundle.sh
+++ bundle.sh
@@ -9,4 +9,4 @@ npm run build
 mkdir -p tmp/akenza-grafana-connector
 cp -R dist/ tmp/akenza-grafana-connector/
 cd tmp && zip -r akenza-grafana-connector.zip akenza-grafana-connector
-yes | cp akenza-grafana-connector.zip "/Volumes/GoogleDrive/Shared drives/Core/akenza-grafana-connector.zip"
+/bin/cp -rf akenza-grafana-connector.zip "/Volumes/GoogleDrive/Shared drives/Core/akenza-grafana-connector.zip"
